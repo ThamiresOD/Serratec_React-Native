f85c8ed0362b9bae8638b7cd639d2528efd27a61
Thamires Ouverney <touverneydarci@gmail.com>
2022-11-15 10:11:01 -0300

    Aula01

---
+++ Aulas/Aula01/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#363940',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#eeeee4',
+    fontSize: 40,
+  }
+});
+
