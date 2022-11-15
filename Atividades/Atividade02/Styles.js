47e47aead79a984a8844f01e0c6ecf7fab10b74b
Thamires Ouverney <touverneydarci@gmail.com>
2022-11-14 22:05:03 -0300

    Atividade02 - Desafio do FlexBox

--- Atividades/Atividade02/Styles.js
+++ Atividades/Atividade02/Styles.js
@@ -61,7 +61,6 @@ export const Styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    // alignContent: 'space-around',
     flexWrap: 'wrap',
     paddingTop: '10%',
   },
