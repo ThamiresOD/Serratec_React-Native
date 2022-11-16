0d9a5b3a5b0bc2ab8e7f714702efd650f4a41d84
Thamires Ouverney <touverneydarci@gmail.com>
2022-11-16 01:01:01 -0300

    Atividade03 - Spotify incompleto

--- Atividades/Atividade03/App.js
+++ Atividades/Atividade03/App.js
@@ -1,27 +1,112 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { styles } from './style';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
     <>
+
+      <View style={styles.faixa01}></View>
+
+      {/* 1/6 -> Titulo-Menu */}
       <View style={styles.container01}>
-        <Text style={styles.texth1}>👻</Text>
-        <Text style={styles.texth1}>Sua Biblioteca</Text>
-        <View style={styles.container02}>
-          <Ionicons name="search" size={32} color="white" />
-          <Ionicons name="add-outline" size={40} color="white" />
+
+        {/* Perfil e título da página */}
+        <View style={styles.section01}>
+          <Text style={styles.text01}>👻</Text>
+          <Text style={styles.text02}>Sua Biblioteca</Text>
+        </View>
+
+        {/* Icones de pesquisa e mais opções */}
+        <View style={styles.section02}>
+          <Ionicons name="search" size={28} color="white" />
+          <Ionicons name="add-outline" size={32} color="white" />
         </View>
       </View>
-      
+
+      {/* 2/6 -> Filtro */}
       <View style={styles.container02}>
-
-
+        <View style={styles.section03}>
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+        </View>
+        <View style={styles.section04}>
+          <Text style={styles.text03}>Playlists</Text>
+          <Text style={styles.text03}>Criadas Por Você</Text>
+          <Text style={styles.text03}>Indicadas pelo Spo</Text>
+        </View>
       </View>
 
-      <View style={styles.container}>
-        <Text style={styles.texth1}>Buuuh 👻</Text>
+      {/* 3/6 ->  Tocados Recentemente */}
+      <View style={styles.container03}>
+        <View style={styles.section05}>
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+          <Text style={styles.text06}>Tocados Recentemente</Text>
+        </View>
+        <View style={styles.section06}>
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+        </View>
       </View>
+
+
+      {/* 4/6 -> Playlists */}
+      <View style={styles.containerFlex}>
+        <View style={styles.container04}>
+
+          {/* Imagem do álbum */}
+          <View style={styles.section07}>
+            <Text>Imagem do álbum x1</Text>
+          </View>
+
+          {/* Nome da Playlist e do criador */}
+          <View style={styles.section08}>
+            <Text style={styles.text07}>Nome da Playlists</Text>
+            <Text style={styles.text08}>Nome do criador</Text>
+          </View>
+        </View>
+      </View>
+
+
+      {/* 5/6 -> Play (So Young) */}
+      <View style={styles.container05}>
+        <View style={styles.section09}>
+          <Text>Imagem da música</Text>
+        </View>
+
+
+        {/* Nome da música e nome da(o) artista/banda */}
+        <View>
+          <View style={styles.section10}>
+            <Text>So Young</Text>
+            <Text style={styles.text08}>Nome do(a) artista</Text>
+          </View>
+        </View>
+
+        {/* ícones de dispositivos, like e play/pause */}
+        <View style={styles.section11}>
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+        </View>
+
+        {/* Barra de tempo da música já reproduzida */}
+        <View>Barra tempo da música</View> //wrap
+      </View>
+
+
+      {/* 6/6 -> Menu do app*/}
+      <View style={styles.container06}>
+        <View style={styles.section12}>
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+          <Ionicons name="ios-close-circle-outline" size={28} color="white" />
+        </View>
+        <View style={styles.section13}>
+          <Text style={styles.text09}>Inicio</Text>
+          <Text style={styles.text09}>Buscar</Text>
+          <Text style={styles.text09}>Sua Biblioteca</Text>
+        </View>
+      </View>
+
     </>
   );
 }
--- Atividades/Atividade03/style.js
+++ Atividades/Atividade03/style.js
@@ -2,40 +2,254 @@ import { StyleSheet } from 'react-native'
 
 
 export const styles = StyleSheet.create({
-    container01: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: '#141414',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#121212',
-      height: '10%',
-    },
 
-    container02: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: '#3939',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        height: '60%',
-        width: '20%'
-    },
+  faixa01: {
+    backgroundColor: '#000',
+    height: 32,
+  },
 
-    texth1: {
-      color: '#eeeee4',
-      fontSize: 30,
-    },
-    texth2: {
-        color: '#eeeee4',
-        fontSize: 25,
-    },
-    button: {
-        backgroundColor: '#1e81b0',
-        padding: 8,
-        marginTop: 20,
-        borderRadius: 5,
-        color: '#eeeee4',
-        justifyItem: 'center',
-    }
+  // 1/6 -> Titulo-Menu 
+  container01: {
+    flexDirection: 'row',
+    backgroundColor: '#141414',
+    height: '10%',
+    width: '100%',
+  },
+  
+  section01: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'end',
+    justifyContent: 'space-evenly',
+    height: '100%',
+    width: '70%',
+    padding: '3%',
+  },
+  
+  text01: {
+    color: '#eeeee4',
+    fontSize: 25,
+    margin: '2%',
+  },
+  
+  text02: {
+    color: '#eeeee4',
+    fontSize: 25,
+    margin: '1%',
+    marginStart: '1%',
+  },
+
+  section02: {
+    flexDirection: 'row',
+    backgroundColor: '#391423',
+    alignItems: 'end',
+    justifyContent: 'space-evenly',
+    height: '100%',
+    width: '30%',
+    paddingBottom: '3%',
+    paddingStart: '5%',
+
+  },
+  
+  /* *************************************** */
+
+  //2/6 -> Filtro 
+  container02: {
+    flexDirection: 'row',
+    backgroundColor: '#3939',
+    alignItems: 'center',
+    height: '5%',
+    width: '100%',
+  },
+
+  section03: {
+    backgroundColor: '#141414',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '15%',
+  },
+
+  section04: {
+    flexDirection: 'row',
+    backgroundColor: '#3939',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    height: '100%',
+    width: '85%',
+  },
+
+  text03: {
+    color: '#eeeee4',
+    fontSize: 15,
+    borderRadius: 5,
+    borderColor: '#141414',
+  },
+
+  /* *************************************** */
+
+  //3/6 ->  Tocados Recentemente
+
+  container03: {
+    flexDirection: 'row',
+    backgroundColor: '#3939',
+    alignItems: 'center',
+    height: '5%',
+    width: '100%',
+    borderTopWidth: 1,
+    borderStartColor: '#000',
+  },
+
+  section05: {
+    flexDirection: 'row',
+    backgroundColor: '#391423',
+    alignItems: 'center',
+    justifyContent: 'start',
+    height: '100%',
+    width: '70%',
+    padding: '3%',
+  },
+
+  text06: {
+    color: '#eeeee4',
+    fontSize: 12,
+    margin: '4%',
+  },
+
+  section06: {
+    backgroundColor: '#141414',
+    alignItems: 'end',
+    justifyContent: 'center',
+    height: '100%',
+    width: '30%',
+    padding: '4%',
+  },
+
+  /* *************************************** */
+
+  //4/6 -> Playlists
+
+  containerFlex: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'center',
+    height: '50%',
+    width: '100%',
+  },
+
+  container04: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'center',
+    height: '10%',
+    width: '100%',
+  },
+
+  section07: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'center',
+    justifyContent: 'start',
+    height: '100%',
+    width: '25%',
+    padding: '3%',
+  },
+
+  section08: {
+    backgroundColor: '#3939',
+    alignItems: 'start',
+    justifyContent: 'center',
+    height: '100%',
+    width: '75%',
+    padding: '1%',
+  },
+
+  text07: {
+    color: '#eeeee4',
+    fontSize: 12,
+  },
+
+  text08: {
+    color: '#eeeee4',
+    fontSize: 10,
+  },
+
+  /* *************************************** */
+
+  //5/6 -> Play (So Young) 
+
+  container05: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'center',
+    height: '10%',
+    width: '100%',
+  },
+  
+  section09: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'center',
+    justifyContent: 'start',
+    height: '100%',
+    width: '25%',
+    padding: '3%',
+  },
+
+  section10: {
+    backgroundColor: '#3939',
+    alignItems: 'start',
+    justifyContent: 'center',
+    height: '100%',
+    width: '75%',
+    padding: '1%',
+  },
+
+  section11: {
+    flexDirection: 'row',
+    backgroundColor: '#393945',
+    alignItems: 'center',
+    justifyContent: 'start',
+    height: '100%',
+    width: '25%',
+    padding: '3%',
+  },
+
+ /* *************************************** */
+
+  //6/6 -> Menu do app
+
+  container06: {
+    backgroundColor: '#3939',
+    alignItems: 'center',
+    height: '8%',
+    width: '100%',
+  },
+  
+  section12: {
+    flexDirection: 'row',
+    backgroundColor: '#3939',
+    alignItems: 'start',
+    justifyContent: 'center',
+    height: '50%',
+    width: '100%',
+    padding: '2%',
+    marginStart:  '5%',
+  },
+
+  section13: {
+    flexDirection: 'row',
+    backgroundColor: '#3939',
+    alignItems: 'start',
+    justifyContent: 'center',
+    height: '50%',
+    width: '100%',
+    padding: '2%',
+  },
+
+  text09: {
+    color: '#eeeee4',
+    fontSize: 10,
+    marginStart:  '5%',
+  },
 })
